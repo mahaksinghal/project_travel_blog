71b92bc8ad17bc524fcdd0e23ad85b9a0615b24c
mahak <mahaksinghal567@gmail.com>
2024-08-13 18:34:30 +0530

    chore: Implement user authentication and registration functionality

--- main/views.py
+++ main/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index (request):
     return render(request, "index.html")
 
 def login_view (request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('index')
     return render(request, "login.html")
 
 def register_view(request):
+    if request.method =="POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassowrd = request.POST.get('cpassowrd')
+        if password == cpassowrd:
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            return redirect('login')
     return render(request, "register.html")
 
 def logout_view(request):
-    return redirect(request, "index.html")
+    logout(request)
+    return redirect("index")
