57972cd79d41eb6ddd507accd18467f50281e4c2
mahak <mahaksinghal567@gmail.com>
2024-08-16 14:04:12 +0530

    adding articles

--- blog/models.py
+++ blog/models.py
@@ -5,5 +5,5 @@ class Article(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length = 150)
     content = models.TextField()
-    iamge = models.ImageField(upload_to="articles/")
+    image = models.ImageField(upload_to="articles/")
     created_at = models.DateTimeField(auto_now_add=True)
