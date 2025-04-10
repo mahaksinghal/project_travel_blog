04d2bbad3a60ec953aca0e603021039d2678558b
mahak <mahaksinghal567@gmail.com>
2025-04-10 19:15:18 +0530

    changes

--- config/urls.py
+++ config/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     path("blog/<int:id>/delete", blog_views.delete_article_view, name = "delete_article"),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
